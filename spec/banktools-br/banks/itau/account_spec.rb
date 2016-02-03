05a9e2355b931d126fcee2b123306b945d742b24
Antonio Filho <antonio@compilandosolucao.com.br>
2016-02-03 18:27:07 -0200

    Add more test cases for itau account and agency validation

--- spec/banktools-br/banks/itau/account_spec.rb
+++ spec/banktools-br/banks/itau/account_spec.rb
@@ -3,25 +3,25 @@ require 'spec_helper'
 describe BanktoolsBR::Banks::Itau::Account do
   describe '#verification_digit' do
     it 'returns extracted verification digit from account number' do
-      expect(described_class.new('8655', '138520').verification_digit).to eq('0')
-      expect(described_class.new('0123', '15207-8').verification_digit).to eq('8')
-      expect(described_class.new('6444', '12345-6').verification_digit).to eq('6')
+      expect(described_class.new('1517', '664585').verification_digit).to eq('5')
+      expect(described_class.new('0776', '05412-4').verification_digit).to eq('4')
+      expect(described_class.new('7210', '048133').verification_digit).to eq('3')
     end
   end
 
   describe '#valid?' do
     context 'when account length is valid' do
       it 'returns true if verification digit is valid' do
-        expect(described_class.new('8655', '13852-0')).to be_valid
-        expect(described_class.new('6458', '15207-8')).to be_valid
-        expect(described_class.new('0393', '013529')).to be_valid
+        expect(described_class.new('7210', '04813-3')).to be_valid
+        expect(described_class.new('7153', '02688-0')).to be_valid
+        expect(described_class.new('4533', '407104')).to be_valid
       end
 
       it 'returns false if verification digit is invalid' do
-        expect(described_class.new('8655', '13852-1')).not_to be_valid
-        expect(described_class.new('6458', '15207-3')).not_to be_valid
-        expect(described_class.new('0393', '01352-2')).not_to be_valid
-        expect(described_class.new('0123', '12345-6')).not_to be_valid
+        expect(described_class.new('0383', '81682-2')).not_to be_valid
+        expect(described_class.new('1965', '36483-1')).not_to be_valid
+        expect(described_class.new('0058', '41594-2')).not_to be_valid
+        expect(described_class.new('2954', '11602-1')).not_to be_valid
       end
     end
 
--- spec/banktools-br/banks/itau/agency_spec.rb
+++ spec/banktools-br/banks/itau/agency_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe BanktoolsBR::Banks::Itau::Agency do
   describe '#valid?' do
     it 'returns true if agency length is equal to 4' do
-      expect(described_class.new('6458')).to be_valid
-      expect(described_class.new('8655')).to be_valid
-      expect(described_class.new('0123')).to be_valid
+      expect(described_class.new('7457')).to be_valid
+      expect(described_class.new('7107')).to be_valid
+      expect(described_class.new('1517')).to be_valid
     end
 
     it 'returns false if agency length is not equal to 4' do
