bd6bb352dbc66827cea7792ae901d67c144f2087
Sergio Campos <sergio.campos.silverio@noverde.com.br>
2018-06-19 09:45:03 -0300

    Santander update for method "correct_verification_digit"

--- lib/banktools-br/banks/santander/account.rb
+++ lib/banktools-br/banks/santander/account.rb
@@ -18,7 +18,9 @@ module BanktoolsBR
         # @return [String] the verification digit
         def correct_verification_digit
           digit_calculator = BanktoolsBR::Banks::DigitCalculator.new(full_account_number, [9, 7, 3, 1, 9, 7, 1, 3, 1, 9, 7, 3])
-          digit = 10 - digit_calculator.sum_numbers.to_s.chars.last.to_i
+          digit = digit_calculator.sum_numbers.to_s.chars.last.to_i
+
+          return (10 - digit).to_s if digit != 0
 
           digit.to_s
         end
